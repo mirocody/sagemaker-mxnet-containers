378bf0727a6d0c8591336b83d424fc9d28920b16
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-05-21 17:01:29 -0700

    add more tests

--- scripts/build_all.py
+++ scripts/build_all.py
@@ -16,10 +16,6 @@ import argparse
 import os
 import subprocess
 
-# PY2_CPU_BINARY = 'https://files.pythonhosted.org/packages/71/64/49c5125befd5e0f0e17f115d55cb78080adacbead9d19f253afd0157656a/mxnet-1.3.0.post0-py2.py3-none-manylinux1_x86_64.whl'  # noqa
-# PY3_CPU_BINARY = 'https://files.pythonhosted.org/packages/71/64/49c5125befd5e0f0e17f115d55cb78080adacbead9d19f253afd0157656a/mxnet-1.3.0.post0-py2.py3-none-manylinux1_x86_64.whl'  # noqa
-# PY2_GPU_BINARY = 'https://files.pythonhosted.org/packages/52/8b/3f9cfe199e592b0723617e5d1919b83022f05cc359bd1885f7d1e9ce4758/mxnet_cu90mkl-1.3.0.post0-py2.py3-none-manylinux1_x86_64.whl'  # noqa
-# PY3_GPU_BINARY = 'https://files.pythonhosted.org/packages/52/8b/3f9cfe199e592b0723617e5d1919b83022f05cc359bd1885f7d1e9ce4758/mxnet_cu90mkl-1.3.0.post0-py2.py3-none-manylinux1_x86_64.whl'  # noqa
 DEFAULT_REGION = 'us-west-2'
 
 
@@ -30,21 +26,11 @@ def _parse_args():
     parser.add_argument('--version')
     parser.add_argument('--repo')
     parser.add_argument('--region', default=DEFAULT_REGION)
-    # parser.add_argument('--py2-cpu-binary', type=str, default=PY2_CPU_BINARY)
-    # parser.add_argument('--py3-cpu-binary', type=str, default=PY3_CPU_BINARY)
-    # parser.add_argument('--py2-gpu-binary', type=str, default=PY2_GPU_BINARY)
-    # parser.add_argument('--py3-gpu-binary', type=str, default=PY3_GPU_BINARY)
 
     return parser.parse_args()
 
 
 args = _parse_args()
-# binaries = {
-#     'py2-cpu': args.py2_cpu_binary,
-#     'py3-cpu': args.py3_cpu_binary,
-#     'py2-gpu': args.py2_gpu_binary,
-#     'py3-gpu': args.py3_gpu_binary
-# }
 
 build_dir = os.path.join('docker', args.version, 'final')
 prev_dir = os.getcwd()
@@ -58,12 +44,6 @@ subprocess.check_call(login_cmd.split())
 
 for arch in ['cpu', 'gpu']:
     for py_version in ['2', '3']:
-        # binary_url = binaries['py{}-{}'.format(py_version, arch)]
-        # binary_file = os.path.basename(binary_url)
-        # cmd = 'wget -O {} {}'.format(binary_file, binary_url)
-        # print('Downloading binary file: {}'.format(cmd))
-        # subprocess.check_call(cmd.split())
-
         tag = '{}-{}-py{}'.format(args.version, arch, py_version)
         dest = '{}:{}'.format(args.repo, tag)
         prev_image_uri = '{}.dkr.ecr.{}.amazonaws.com/{}'.format(args.account, args.region, dest)
@@ -79,14 +59,10 @@ for arch in ['cpu', 'gpu']:
             '--cache-from', prev_image_uri,
             '--build-arg', 'py_version={}'.format(py_version),
             '--build-arg', 'framework_support_installable={}'.format(tar_file),
-            # '--build-arg', 'framework_installable={}'.format(binary_file),
             '-t', dest,
             '.',
         ]
         print('Building docker image: {}'.format(' '.join(build_cmd)))
         subprocess.check_call(build_cmd)
-
-        # print('Deleting binary file {}'.format(binary_file))
-        # subprocess.check_call('rm {}'.format(binary_file).split())
 
 os.chdir(prev_dir)
